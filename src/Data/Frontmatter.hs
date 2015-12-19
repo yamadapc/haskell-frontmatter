809874a3b97f17b287cb1ba0219e1d2a49ff1d2b
yamadapc <tacla.yamada@gmail.com>
2015-12-19 21:03:01 -0200

    Some small polish to publishing to hackage

--- src/Data/Frontmatter.hs
+++ src/Data/Frontmatter.hs
@@ -26,15 +26,34 @@ import           Data.Frontmatter.Internal
 import           Data.Yaml                  (FromJSON)
 import           Data.Yaml.Frontmatter
 
+-- |
+-- Parse a frontmatter from a 'ByteString' returning a 'Result'. Just extracts
+-- whatever is on the frontmatter; doesn't care what it is.
+parseFrontmatter :: ByteString -> Result ByteString
 parseFrontmatter = parse frontmatter
+
+-- |
+-- 'parseFrontmatter' but returning a 'Maybe'
+parseFrontmatterMaybe :: ByteString -> Maybe ByteString
 parseFrontmatterMaybe = maybeResult . parse frontmatter
+
+-- |
+-- 'parseFrontmatter' but returning an 'Either'
+parseFrontmatterEither :: ByteString -> Either String ByteString
 parseFrontmatterEither = eitherResult . parse frontmatter
 
+-- |
+-- Parse a frontmatter from a 'ByteString' returning a 'FromJSON a'. Will parse
+-- both JSON and YAML.
 parseYamlFrontmatter :: FromJSON a => ByteString -> Result a
 parseYamlFrontmatter = parse frontmatterYaml
 
+-- |
+-- 'parseYamlFrontmatter' but returning a 'Maybe'
 parseYamlFrontmatterMaybe :: FromJSON a => ByteString -> Maybe a
 parseYamlFrontmatterMaybe = maybeResult . parse frontmatterYaml
 
+-- |
+-- 'parseYamlFrontmatter' but returning an 'Either'
 parseYamlFrontmatterEither :: FromJSON a => ByteString -> Either String a
 parseYamlFrontmatterEither = eitherResult . parse frontmatterYaml
